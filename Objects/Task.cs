8ddeeb529f81e305efe81a636ac2daa4e1e29331
jill <jillkuchman@gmail.com>
2016-02-24 09:01:54 -0800

    Add methods and passing tests to Task class

--- Objects/Task.cs
+++ Objects/Task.cs
@@ -15,6 +15,21 @@ namespace ToDoList
       _description = Description;
     }
 
+    public override bool Equals(System.Object otherTask)
+    {
+        if (!(otherTask is Task))
+        {
+          return false;
+        }
+        else
+        {
+          Task newTask = (Task) otherTask;
+          bool idEquality = (this.GetId() == newTask.GetId());
+          bool descriptionEquality = (this.GetDescription() == newTask.GetDescription());
+          return (idEquality && descriptionEquality);
+        }
+    }
+
     public int GetId()
     {
       return _id;
@@ -54,8 +69,75 @@ namespace ToDoList
       {
         conn.Close();
       }
-
       return allTasks;
     }
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO tasks (description) OUTPUT INSERTED.id VALUES (@TaskDescription);", conn);
+
+      SqlParameter descriptionParameter = new SqlParameter();
+      descriptionParameter.ParameterName = "@TaskDescription";
+      descriptionParameter.Value = this.GetDescription();
+      cmd.Parameters.Add(descriptionParameter);
+      rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM tasks;", conn);
+      cmd.ExecuteNonQuery();
+    }
+    public static Task Find(int id)
+    {
+      SqlConnection conn = DB.Connection();
+      SqlDataReader rdr = null;
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM tasks WHERE id = @TaskId;", conn);
+      SqlParameter taskIdParameter = new SqlParameter();
+      taskIdParameter.ParameterName = "@TaskId";
+      taskIdParameter.Value = id.ToString();
+      cmd.Parameters.Add(taskIdParameter);
+      rdr = cmd.ExecuteReader();
+
+      int foundTaskId = 0;
+      string foundTaskDescription = null;
+      while(rdr.Read())
+      {
+        foundTaskId = rdr.GetInt32(0);
+        foundTaskDescription = rdr.GetString(1);
+      }
+      Task foundTask = new Task(foundTaskDescription, foundTaskId);
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+      return foundTask;
+    }
+
   }
 }
