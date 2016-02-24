9c2537a9ea8fa43727664e4e883adf6aa764d489
jill <jillkuchman@gmail.com>
2016-02-24 09:32:08 -0800

    Edit typo: categoryId to category_id column name

--- Objects/Category.cs
+++ Objects/Category.cs
@@ -146,7 +146,7 @@ namespace ToDoList
       SqlDataReader rdr = null;
       conn.Open();
 
-      SqlCommand cmd = new SqlCommand("SELECT * FROM tasks WHERE categoryId = @CategoryId;", conn);
+      SqlCommand cmd = new SqlCommand("SELECT * FROM tasks WHERE category_id = @CategoryId;", conn);
       SqlParameter categoryIdParameter = new SqlParameter();
       categoryIdParameter.ParameterName = "@CategoryId";
       categoryIdParameter.Value = this.GetId();
